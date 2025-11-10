e29f9230bd0948343fe71f07934c925acb9d8d72
shainas786 <shainasparveen786@gmail.com>
2025-11-10 21:20:04 +0530

    Add files via upload

---
+++ corejava/src/daytwelve/Checkedexception.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwelve;
+
+import java.io.IOException;
+
+public class Checkedexception {
+
+	public static void main(String[] args) throws IOException {
+		String str = CheckedexceptionDemo.acceptText();// reads line from console
+		System.out.println("Your string is " + str);
+
+	}
+
+}
---
+++ corejava/src/daytwelve/CheckedexceptionDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.daytwelve;
+
+import java.io.IOException;
+
+public class CheckedexceptionDemo {
+
+	public static void main(String[] args) throws IOException {
+		Object Checkedexception = null;
+		Object str = ((Object) Checkedexception);// reads line from console
+		System.out.println("Your string is " + str);
+
+	}
+
+	public static String acceptText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
